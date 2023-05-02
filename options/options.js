e7edfecf0a724cc6b994fdecfdb67a9445e14a37
Crowdbotics <team@crowdbotics.com>
2023-05-02 08:29:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-glitter-40392",
+  url: "https://fragrant-glitter-40392.botics.co",
+  api: "https://fragrant-glitter-40392.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
